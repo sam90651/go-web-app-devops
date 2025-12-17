cb21130fd70378f1c04c11cbda28fc89145dc769
sam90651 <sumitmilindb@gmail.com>
2025-12-16 21:53:43 -0600

    Update main.go

--- main.go
+++ main.go
@@ -1,39 +1,56 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
 	"log"
 	"net/http"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	// Render the home html page from static folder
-	http.ServeFile(w, r, "static/home.html")
-}
-
-func coursePage(w http.ResponseWriter, r *http.Request) {
-	// Render the course html page
-	http.ServeFile(w, r, "static/courses.html")
-}
-
-func aboutPage(w http.ResponseWriter, r *http.Request) {
-	// Render the about html page
-	http.ServeFile(w, r, "static/about.html")
-}
-
-func contactPage(w http.ResponseWriter, r *http.Request) {
-	// Render the contact html page
-	http.ServeFile(w, r, "static/contact.html")
-}
+// Embed everything inside static/
+//go:embed static/*
+var staticFiles embed.FS
 
 func main() {
+	mux := http.NewServeMux()
 
-	http.HandleFunc("/home", homePage)
-	http.HandleFunc("/courses", coursePage)
-	http.HandleFunc("/about", aboutPage)
-	http.HandleFunc("/contact", contactPage)
+	// Serve specific pages
+	mux.HandleFunc("/home", serveEmbeddedHTML("home.html"))
+	mux.HandleFunc("/courses", serveEmbeddedHTML("courses.html"))
+	mux.HandleFunc("/about", serveEmbeddedHTML("about.html"))
+	mux.HandleFunc("/contact", serveEmbeddedHTML("contact.html"))
 
-	err := http.ListenAndServe("0.0.0.0:8080", nil)
-	if err != nil {
-		log.Fatal(err)
+	// Optional: redirect / -> /home
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+		http.Redirect(w, r, "/home", http.StatusFound)
+	})
+
+	log.Println("Listening on 0.0.0.0:8080")
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", mux))
+}
+
+func serveEmbeddedHTML(filename string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Only allow exact route path (prevents /home/anything serving same file)
+		// Remove this check if you don't want strict routing.
+		// (Not required for tests, but good practice.)
+		// NOTE: This function is used for multiple routes; so we don't check path here.
+
+		f, err := staticFiles.Open("static/" + filename)
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+		defer f.Close()
+
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+		// Serve content with a stable name for caching headers etc.
+		info, _ := fs.Stat(staticFiles, "static/"+filename)
+		http.ServeContent(w, r, filename, info.ModTime(), f)
 	}
 }
