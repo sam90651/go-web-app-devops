a207739424cc89397295a5ca9891d2d3432fa92f
sam90651 <sumitmilindb@gmail.com>
2025-12-16 21:56:59 -0600

    Update main.go

--- main.go
+++ main.go
@@ -2,19 +2,16 @@ package main
 
 import (
 	"embed"
-	"io/fs"
 	"log"
 	"net/http"
 )
 
-// Embed everything inside static/
 //go:embed static/*
 var staticFiles embed.FS
 
 func main() {
 	mux := http.NewServeMux()
 
-	// Serve specific pages
 	mux.HandleFunc("/home", serveEmbeddedHTML("home.html"))
 	mux.HandleFunc("/courses", serveEmbeddedHTML("courses.html"))
 	mux.HandleFunc("/about", serveEmbeddedHTML("about.html"))
@@ -35,22 +32,14 @@ func main() {
 
 func serveEmbeddedHTML(filename string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// Only allow exact route path (prevents /home/anything serving same file)
-		// Remove this check if you don't want strict routing.
-		// (Not required for tests, but good practice.)
-		// NOTE: This function is used for multiple routes; so we don't check path here.
-
-		f, err := staticFiles.Open("static/" + filename)
+		b, err := staticFiles.ReadFile("static/" + filename)
 		if err != nil {
 			http.NotFound(w, r)
 			return
 		}
-		defer f.Close()
 
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-
-		// Serve content with a stable name for caching headers etc.
-		info, _ := fs.Stat(staticFiles, "static/"+filename)
-		http.ServeContent(w, r, filename, info.ModTime(), f)
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write(b)
 	}
 }
